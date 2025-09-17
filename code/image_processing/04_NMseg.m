2422441939112541dfc78d8d05de29ea3c734cdf
Madhavi Tippani <madhavitippani28@gmail.com>
2025-09-17 11:49:17 -0400

    updated NM seg code to run on all samples

--- code/image_processing/04_NMseg.m
+++ code/image_processing/04_NMseg.m
@@ -1,10 +1,10 @@
-D = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/raw-data/Images/'; 
-myfiles = dir(fullfile(D, '*.tif'));
-matchingFiles = myfiles(contains({myfiles.name}, {'_A1.tif', '_B1.tif', '_C1.tif', '_D1.tif'}));
-O = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/processed-data/Images/NMseg/';
-for i = 1:numel(matchingFiles)
+function NMseg(brnum)
 
-he = imread(fullfile(matchingFiles(i).folder, matchingFiles(i).name));
+disp(brnum)
+Md = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data/xenium_imageProcessing/'
+fname = fullfile(Md,brnum,'HE_registered_to_DAPI.tif')	
+	
+he = imread(fname);
 tic
 disp('Performing rgb to Lab color space conversion')
 lab_he = rgb2lab(he); % convert from rgb color space to Lab color space
@@ -18,14 +18,12 @@ N=2;
 pixel_labels = imsegkmeans(ab,N,'NumAttempts',3); % apply Kmeans
 toc
 
+disp('saving ...')
+	
 parfor ic = 1:N
 mask{ic} = pixel_labels==ic;
 cluster{ic} = he .* uint8(mask{ic});
-imwrite(imresize(cluster{ic}, 0.5), fullfile(O, [matchingFiles(i).name(1:end-4), num2str(ic),'.png']))
+imwrite(cluster{ic}), fullfile(Md, brnum, ['Kmeans_cluster', num2str(ic),'.png']))
 end
 
-NM = cluster{2};
-save(fullfile(O, [matchingFiles(i).name(1:end-4), '.mat']),'NM','-v7.3')
-
-disp(['done ',matchingFiles(i).name])
 end
