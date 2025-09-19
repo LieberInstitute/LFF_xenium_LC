0d6b055edb2e58fb8e094b231465e0aa99d20809
Madhavi Tippani <madhavitippani28@gmail.com>
2025-09-19 06:57:55 -0400

    function call error with cluster

--- code/image_processing/NMseg_thresh.m
+++ code/image_processing/NMseg_thresh.m
@@ -3,9 +3,9 @@ function NMseg_thresh(brnum, K)
 disp(brnum)
 Md = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data/xenium_imageProcessing/'
 fname = fullfile(Md,brnum,'Kmeans_cluster.mat')	
-
-    load(fname);                             
-    NM = cluster{K};                            
+	S = load(fname);  
+	C = S.cluster;                       
+    NM = C{K};                            
     Ie = rgb2gray(NM);
     Ie = mat2gray(Ie);   
 	BW = zeros(size(Ie));
