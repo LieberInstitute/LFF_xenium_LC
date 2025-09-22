fc02c6e75d71935d9dba83aab5f3c9baa09568bb
Madhavi Tippani <madhavitippani28@gmail.com>
2025-09-22 14:17:17 -0400

    final NMseg_removeFolds.m script'

--- code/image_processing/NMseg_removeFolds.m
+++ code/image_processing/NMseg_removeFolds.m
@@ -3,6 +3,8 @@ function Nmseg_removeFolds(brnum, K)
 disp(brnum)
 Md = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data/xenium_imageProcessing/'
 fname = fullfile(Md,brnum,'Kmeans_cluster.mat')	
+	
+	load(fullfile(Md,brnum,'NMthresh.mat'),  "BW1")
 	S = load(fname);  
 	C = S.cluster;                       
     NM = C{K};                            
@@ -11,37 +13,15 @@ fname = fullfile(Md,brnum,'Kmeans_cluster.mat')
 	Ie(Ie<0.1) = 0;
 	Ie(Ie>0.5) = 0; 
 	
-	initial_mask = imbinarize(gray_image, 'adaptive');
+	initial_mask = imbinarize(Ie, 'adaptive');
 	cleaned_mask = bwareaopen(initial_mask, 100);
-	folds_mask = bwareaopen(cleaned_mask, 25000);
+	folds_mask = bwareaopen(cleaned_mask, 30000);
 	BWrg = region_grow_intensity(Ie, folds_mask, 0.01, 0.5);
+	BW_nofolds = BW1;
+	BW_nofolds(BWrg==1) = 0;
+	save(fullfile(Md,brnum,'NMnofolds.mat'), "BW_nofolds")
 	
-	
-		imwrite(BWrg, fullfile(Md,brnum,'Nmthresh_nofolds.png'))	
+	imwrite(BW_nofolds, fullfile(Md,brnum,'NMnofolds.png'))	
 	
 end
-		
-		%%% testing
-				
-%bad = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data/xenium_imageProcessing/Br1039-L/Kmeans_cluster.mat';  
-%good = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data/xenium_imageProcessing/Br0946/Kmeans_cluster.mat'; 
-%load(bad)
-%	B = cluster{1};
-%load(good)   
-%	G = cluster{1};
-%
-%GIe = mat2gray(rgb2gray(G));
-%BIe = mat2gray(rgb2gray(B));
-%
-%BWG = zeros(size(GIe)); BWG(GIe>0.05 & GIe<graythresh(GIe)) =1;
-%BWB = zeros(size(BIe)); BWB(BIe>0.05 & BIe<graythresh(BIe)) =1;
-%
-%Bc = imopen(BWB, strel('disk', 3)); 
-%BcN = imclose(Bc, strel('disk', 3)); 
-%bwB = bwareaopen(BcN, 500);
-%
-%Gc = imopen(BWG, strel('disk', 3)); 
-%GcN = imclose(Gc, strel('disk', 3)); 
-%bwG = bwareaopen(GcN, 500);
-%
 
