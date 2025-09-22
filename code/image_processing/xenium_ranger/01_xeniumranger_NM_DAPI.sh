d03940edbf5863dd78c978df4b4544039131e4dd
Madhavi Tippani <madhavitippani28@gmail.com>
2025-09-22 15:05:16 -0400

    run xenium ranger samples 4 & 9

--- code/image_processing/xenium_ranger/01_xeniumranger_NM_DAPI.sh
+++ code/image_processing/xenium_ranger/01_xeniumranger_NM_DAPI.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=24
 #SBATCH -o 01_xeniumranger_NM_DAPI_%a.txt
 #SBATCH -e 01_xeniumranger_NM_DAPI_%a.txt
-#SBATCH --array=5-8%5
+#SBATCH --array=9
 #SBATCH --constraint="intel"
 
 echo "**** Job starts ****"
