0c649555977258de633d62a7438b7926fa4da8e3
Madhavi Tippani <madhavitippani28@gmail.com>
2025-09-22 14:18:33 -0400

    updated 01_xeniumranger_NM_DAPI.sh run all samples

--- code/image_processing/xenium_ranger/01_xeniumranger_NM_DAPI.sh
+++ code/image_processing/xenium_ranger/01_xeniumranger_NM_DAPI.sh
@@ -2,14 +2,13 @@
 #SBATCH --mem=285G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH -o /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/xenium_imageProcessing/xenium_ranger/01_xeniumranger_NM_DAPI.txt
-#SBATCH -e /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/xenium_imageProcessing/xenium_ranger/01_xeniumranger_NM_DAPI.txt
-#SBATCH --array=1
+#SBATCH -o 01_xeniumranger_NM_DAPI_%a.txt
+#SBATCH -e 01_xeniumranger_NM_DAPI_%a.txt
+#SBATCH --array=1-3%3
 #SBATCH --constraint="intel"
 
 echo "**** Job starts ****"
 date
-
 
 echo "**** JHPCE info ****"
 echo "User: ${USER}"
@@ -20,15 +19,23 @@ echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
 module --ignore_cache load xeniumranger/3.1.1
 
-# get current sample to resegment
-#FILE=/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/raw-data/xenium/output-XETG00558__0068654__Br6297__20250501__172909
-FILE=/dcs04/lieber/lcolladotor/rawDataTDSC_LIBD001/raw-data/_xenium_LIBD/250505_run-2/output-XETG00558__0068654__Br6297__20250501__172909
-MASK=/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/processed-data/xenium_imageProcessing/Br6297/combined_nucmask.npy
-OUTPATH=xeniumranger_NM_DAPI
+BASE="/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC"
 
-xeniumranger import-segmentation --xenium-bundle=${FILE} --id=${OUTPATH} --nuclei=${MASK} --localcores=48 --localmem=250 --disable-ui=true --jobmode=local
+# Get brnum from Nth line (tab-delimited). Stops after first print.
+brnum=$(awk -v n="$SLURM_ARRAY_TASK_ID" 'BEGIN{FS="\t"} NR==n {print $1}' ../inputs.txt | awk '{print $1}')
+echo "brnum = $brnum"
+
+bundle=$(awk -v n="$SLURM_ARRAY_TASK_ID" 'BEGIN{FS="\t"} NR==n {print $6}' ../inputs.txt | awk '{print $1}')
+echo "bundle = $bundle"
+
+
+MASK=${BASE}/processed-data/xenium_imageProcessing/${brnum}/combined_nucmask.npy
+BUNDLE=${BASE}/raw-data/xenium/xenium-instrument/${bundle}
+OUTPATH=xeniumranger_NM_DAPI_${brnum}
+
+xeniumranger import-segmentation --xenium-bundle=${BUNDLE} --id=${OUTPATH} --nuclei=${MASK} --localcores=48 --localmem=250 --disable-ui=true --jobmode=local
 
 # move output where it should've been able to go in the first place
-mv ${OUTPATH} /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/processed-data/xenium_imageProcessing/Br6297/
+mv ${OUTPATH} ${BASE}/processed-data/xenium_imageProcessing/${brnum}/
 echo "**** Job ends ****"
 date
