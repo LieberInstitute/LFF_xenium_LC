41f23c702b98341c5fc568dbef65b20ca722647c
Madhavi Tippani <madhavitippani28@gmail.com>
2026-01-23 08:35:28 -0500

    adjust threshold to pick tissue only pixeks from slide image

--- code/image_processing/NMscore.m
+++ code/image_processing/NMscore.m
@@ -6,27 +6,21 @@ cd '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC'
 	opts.DataLines = [1 Inf];     % <- don’t skip first 4 lines
 	opts.CommentStyle = {};       % <- don’t drop lines starting with #, etc.
 	samples = readtable(fname, opts);
-	
-files = dir(fullfile(pwd, '/processed-data/xenium_imageProcessing/'));
-myfiles = files(cellfun(@(x) length(x) == 17, {files.name}));
 
-%% bernie annotation based BG 
-for i= 1:numel(myfiles)
-fname = myfiles(i).name(1:end-4);
-disp(fname);
-img = imread([pwd, '/raw-data/Images/',fname,'.tif']);
-
-NMseg_dir = fullfile(pwd, '/processed-data/Images/NMseg/');
-load([NMseg_dir, fname, 'NMseg_clean.mat'])
+for i= 1:size(samples,1)	
+	disp(samples.Var1{i})
+		
+img = imread(fullfile(pwd, '/processed-data/xenium_imageProcessing/', samples.Var1{i}, 'HE_registered_to_DAPI.tif'));
+load(fullfile(pwd, '/processed-data/xenium_imageProcessing/', samples.Var1{i}, 'NMnofolds.mat'))
 
 img1 = mat2gray(rgb2gray(img));
 BW = img1<0.8&img1>0.1;     
 img1(~BW)=0; 
 	
-BG_mask = img1; BG_mask(NM) = 0; 
-HE = mean(BG_mask>0);
+BG_mask = img1; BG_mask(BW_nofolds) = 0; temp=BG_mask(BG_mask>0);
+HE = mean(BG_mask(:));
 
-NM_mask = img1; NM_mask(~NM) = 0; 
+NM_mask = img1; NM_mask(~BW_nofolds) = 0; 
 nm = NM_mask(NM_mask>0);
 Pi = sum(nm-HE);
 maxNM = max(nm-HE);
