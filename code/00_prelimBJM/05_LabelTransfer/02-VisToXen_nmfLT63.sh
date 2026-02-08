001cc87a8fbd37698f1590c7beff06391e2f2586
Bernie Mulvey <berniejmulvey@gmail.com>
2026-02-08 16:39:03 -0600

    code for nmfLabelTransfer with crossval-selected number of factors

---
+++ code/00_prelimBJM/05_LabelTransfer/02-VisToXen_nmfLT63.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=cv
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=80G
+#SBATCH --output=logs/02_nmfLT63.txt
+#SBATCH --error=logs/02_nmfLT63.txt
+#SBATCH --mail-type=END
+#SBATCH --mail-user=bernie.mulvey@libd.org
+#SBATCH --time=4-00:00:00
+
+echo "**** Job starts ****"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.3.x
+
+## List current modules for reproducibility
+module list
+
+## NMF script
+Rscript 02-VisToXen_nmfLabelTransfer_k63.R
+
+echo "**** Job ends ****"
+date
