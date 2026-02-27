8eca6df819fd53464a58ffddaf6547d83ac2c4da
Bernie Mulvey <berniejmulvey@gmail.com>
2026-02-26 18:29:32 -0600

    rearrange domain label transfer and smoothing for an extension of visium sensitivity analysis (i.e., label transfer of reprocessed visium -> smoothing of LC domain and domain DE)

---
+++ code/99_UsingVisSensitivityDomains/01_labelTransfer/05b-LCdons_VisToXen_nmfLT63.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=cv
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=300G
+#SBATCH --output=logs/05b_nmfLT63.txt
+#SBATCH --error=logs/05b_nmfLT63.txt
+#SBATCH --mail-type=END
+#SBATCH --mail-user=bernie.mulvey@libd.org
+#SBATCH --time=4-00:00:00
+
+echo "**** Job starts ****"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.3.x
+
+## List current modules for reproducibility
+module list
+
+## NMF script
+Rscript 05b-LCdonsonly_VisToXen_nmfLabelTransfer.R
+
+echo "**** Job ends ****"
+date
