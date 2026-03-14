d95bc436a2615048799814284b2360dfd22f58be
Bernie Mulvey <berniejmulvey@gmail.com>
2026-03-14 14:15:37 -0500

    Set up banksy runs with banksy dimred outputs

--- code/04_Banksy_clusterings/01b-ManyBanksy.sh
+++ code/04_Banksy_clusterings/01b-ManyBanksy.sh
@@ -3,9 +3,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=130G
-#SBATCH --array=1-48
-#SBATCH -o /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data/00_prelim_procdat_BJM/04_Banksy_clusterings/01-banksy_clustruns_outs/logs/ManyBanksy_%a.out
-#SBATCH -e /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data/00_prelim_procdat_BJM/04_Banksy_clusterings/01-banksy_clustruns_outs/logs/ManyBanksy_%a.err
+#SBATCH --array=1-24
+#SBATCH -o /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data/04_Banksy_clusterings/01-banksy_clustruns_outs/logs/ManyBanksy_%a.out
+#SBATCH -e /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data/04_Banksy_clusterings/01-banksy_clustruns_outs/logs/ManyBanksy_%a.err
 
 echo "**** JHPCE info ****"
 echo "User: ${USER}"
@@ -16,8 +16,8 @@ echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
 module load conda_R/4.4
 
-## run this script from code dir
-cd /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/code/00_prelimBJM/04_Banksy_clusterings
+## run this script from code dir. each R process will read in a table of parameters and take the job array #s row of parameters to run banksy w
+cd /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/code/04_Banksy_clusterings
 Rscript --no-save --no-restore 01b-Many_Banksy.R
 
 echo "**** Job ends ****"
