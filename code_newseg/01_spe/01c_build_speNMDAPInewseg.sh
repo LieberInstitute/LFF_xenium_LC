de3f9f39a049e64b3a393e3e03dc85ce3a5155d2
Bernie Mulvey <berniejmulvey@gmail.com>
2026-06-03 15:10:43 -0400

    manual LC definitions, setup for web-div snseq label xfer

---
+++ code_newseg/01_spe/01c_build_speNMDAPInewseg.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --mem=60G
+#SBATCH --job-name=buildSPE_DAPI
+#SBATCH -c 1
+#SBATCH -t 1-00:00:00
+#SBATCH -o logs/buildSPE_DAPI_%a.txt
+#SBATCH -e logs/buildSPE_DAPI_%a.txt
+#SBATCH --array=34
+
+set -e
+
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_CLUSTER_NAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load Modules
+module load conda_R/4.3.x
+
+## List current modules for reproducibility
+module list
+
+## Run code
+brnum=$(awk -v n="$SLURM_ARRAY_TASK_ID" 'BEGIN{FS="\t"} NR==n {print $1}' /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/code/xenium_imageProcessing_new/inputs.txt | awk '{print $1}')
+echo "$brnum"
+
+export BRNUM=$brnum
+Rscript 01c_build_speNMDAPInewseg.R
+
+
+## Memeory stat
+#sstat -a -o JobID,MaxVMSizeNode,MaxVMSize,AveVMSize,MaxRSS,AveRS S,MaxDiskRead,MaxDiskWrite,AveCPUFreq,TRESUsageInMax -j ${SLURM_JOB_ID}
+
+echo "**** Job ends ****"
+date
---
+++ processed-data_newseg/02_QC/readme.md
@@ -0,0 +1,8 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2868
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 processed-data files 06 and 07 are QCed and dimensionality-reduced  SPEs using data as output by Xenium using its default DAPI-only segmentations (i.e., no NM segmentation was involved). These files were therefore not regenerated in the "newseg" run and are found in processed-data/02_QC/ .}
