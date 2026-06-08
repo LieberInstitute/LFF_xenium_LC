fc91b85474ee29a7dde8690447d02ff94c6b891d
Madhavi Tippani <madhavitippani28@gmail.com>
2026-06-08 17:09:37 -0400

    shell scripts for NMseg workflow

---
+++ code/xenium_imageProcessing_new/04d_NMseg_removeFolds.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --mem=40G
+#SBATCH --job-name=NMseg_removeFolds
+#SBATCH -c 1
+#SBATCH -t 1-00:00:00
+#SBATCH -o logs/NMseg_removeFolds_%a.txt
+#SBATCH -e logs/NMseg_removeFolds_%a.txt
+#SBATCH --array=1-34%5
+
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load matlab
+module list
+
+# --------- CONFIG ---------
+
+brnum=$(awk -v n="$SLURM_ARRAY_TASK_ID" 'BEGIN{FS="\t"} NR==n {print $1}' inputs.txt | awk '{print $1}')
+echo "$brnum"
+K=$(awk -v n="$SLURM_ARRAY_TASK_ID" 'BEGIN{FS="\t"} NR==n {print $4}' inputs.txt | awk '{print $1}')
+echo "$K"
+
+matlab -nodesktop -nosplash -r "NMseg_removeFolds('$brnum',$K)"
+
+echo "**** Job ends ****"
+date
