52698401f330a9abf724bda868ef9cad47a56354
Madhavi Tippani <madhavitippani28@gmail.com>
2026-06-08 17:09:07 -0400

    xeniumRanger.sh for new data

---
+++ code/xenium_imageProcessing_new/xeniumRanger.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --mem=285G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH -o logs/01_xeniumranger_NM_DAPI_%a.txt
+#SBATCH -e logs/01_xeniumranger_NM_DAPI_%a.txt
+#SBATCH --array=1-34%5
+#SBATCH --constraint="intel"
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module --ignore_cache load xeniumranger/3.1.1
+
+BASE="/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC"
+
+# Get brnum from Nth line (tab-delimited). Stops after first print.
+brnum=$(awk -v n="$SLURM_ARRAY_TASK_ID" 'BEGIN{FS="\t"} NR==n {print $1}' inputs.txt | awk '{print $1}')
+echo "brnum = $brnum"
+
+bundle=$(awk -v n="$SLURM_ARRAY_TASK_ID" 'BEGIN{FS="\t"} NR==n {print $6}' inputs.txt | awk '{print $1}')
+echo "bundle = $bundle"
+
+
+MASK=${BASE}/processed-data/xenium_imageProcessing_new/${brnum}/combined_nucmask.npy
+BUNDLE=${BASE}/raw-data/xenium/xenium-instrument/${bundle}
+OUTPATH=xeniumranger_NM_DAPI_${brnum}
+
+xeniumranger import-segmentation --xenium-bundle=${BUNDLE} --id=${OUTPATH} --nuclei=${MASK} --localcores=48 --localmem=250 --disable-ui=true --jobmode=local
+
+# move output where it should've been able to go in the first place
+mv ${OUTPATH} ${BASE}/processed-data/xenium_imageProcessing_new/xeniumRanger/
+echo "**** Job ends ****"
+date
