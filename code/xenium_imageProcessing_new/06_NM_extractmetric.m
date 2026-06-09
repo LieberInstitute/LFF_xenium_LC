5b033d622ad5c046e7953f97d9a827a4b2fd1bb6
Madhavi Tippani <madhavitippani28@gmail.com>
2026-06-09 17:39:52 -0400

    fix typos and run 06_NM_extractmetric.m

--- code/xenium_imageProcessing_new/06_NM_extractmetric.m
+++ code/xenium_imageProcessing_new/06_NM_extractmetric.m
@@ -34,11 +34,26 @@ Md = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC';
 		Rout = imref2d([target_height, target_width]);
 		
 		HE = imread(fullfile(Md,id,[brain,'_registered.png']));
-		BW = imread(fullfile(Md,id,brain,'NMnofolds.png']));
+		BW = imread(fullfile(Md,id,brain,'NMnofolds.png'));
 		BW_reg = imwarp(BW, T, 'OutputView', Rout);
-		temp = regionprop(BW_reg, mat2gray(rgb2gray(HE)), "area", "centroid", "boundingbox", "MeanIntensity", "MaxIntensity", );
-		%out=imoverlay(img_registered,BW, [0,1,0]);
-		%imwrite(imresize(out,0.5), fullfile(fullfile(Md,id,[brain,'_regis_overlay.png'])))
-		imwrite(img_registered, fullfile(fullfile(Md,id,[brain,'_registered.png'])))
+		temp = regionprops(BW_reg, mat2gray(rgb2gray(HE)), "area", "centroid", "boundingbox", "MeanIntensity", "MaxIntensity");			
+			Tprops = struct2table(temp);
+
+			% Optional: split centroid and bbox into separate columns
+			if ~isempty(Tprops)
+			    Tprops.Centroid_X = Tprops.Centroid(:,1);
+			    Tprops.Centroid_Y = Tprops.Centroid(:,2);
+			    Tprops.BBox_X      = Tprops.BoundingBox(:,1);
+			    Tprops.BBox_Y      = Tprops.BoundingBox(:,2);
+			    Tprops.BBox_Width  = Tprops.BoundingBox(:,3);
+			    Tprops.BBox_Height = Tprops.BoundingBox(:,4);
+			    Tprops.Centroid = [];
+			    Tprops.BoundingBox = [];
+			end
+
+			outcsv = fullfile(Md, id, brain, 'NM_regionprops.csv');
+			writetable(Tprops, outcsv);
+				
 		disp(['done ',brain])	
+			
 	end
