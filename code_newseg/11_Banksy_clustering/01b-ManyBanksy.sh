2eb71e0bfcc4d3f9900f55d4c9489a2b9b62f155
Bernie Mulvey <berniejmulvey@gmail.com>
2026-06-12 15:23:34 -0400

    despite myself, setting up banksy on the newsegs because DE results are doin me the great bamboozle

---
+++ code_newseg/11_Banksy_clustering/01b-ManyBanksy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=BNKS_xLCnewseg
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=130G
+#SBATCH --array=1-24
+#SBATCH -o /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data_newseg/11_Banksy_clustering/01-banksy_clustruns_outs/logs/ManyBanksy_%a.out
+#SBATCH -e /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/processed-data_newseg/11_Banksy_clustering/01-banksy_clustruns_outs/logs/ManyBanksy_%a.err
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+# banksy 1.6.0 in bioc 3.22 (Requiring r 4.5)  has parallelization!
+module load conda_R/4.5
+
+## run this script from code dir. each R process will read in a table of parameters and take the job array #s row of parameters to run banksy w
+cd /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_xenium_LC/code/04_Banksy_clusterings
+Rscript --no-save --no-restore 01b-Many_Banksy.R
+
+echo "**** Job ends ****"
+date
+
